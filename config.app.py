587d62f5fd7feac793314aa4c9e04320d5559734
Pascal Pfiffner <phase.of.matter@gmail.com>
2015-07-13 11:05:53 +0200

    Update medication-app configuration

--- config.app.py
+++ config.app.py
@@ -5,6 +5,8 @@
 #  copy to "config.py"
 
 DEBUG = True
+ONLY_BASIC_REVIEW = True
+IGNORE_REVIEW_CONCLUSION_ERRORS = True		# if true ignores status == 400 (not all 400) raised when sending concluded meds
 
 SESSION_SECRET = "use-your-own"
 API_SERVER_BASE = "http://127.0.0.1:18000"
